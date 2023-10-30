e3b38eedd78cc534a04f316e76e0f7793bc070bf
cftuabbottgithub <149209531+cftuabbottgithub@users.noreply.github.com>
2023-10-30 09:24:05 -0700

    update tests

--- general_store_test.py
+++ general_store_test.py
@@ -16,7 +16,7 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selenium.common.exceptions import ElementNotVisibleException
 
 # locators
-import locators
+from locators import HomePage, ProductPage, CartPage, ProductPageLocators, CartPageLocators
 
 capabilities = dict(
     platformName='Android',
@@ -35,6 +35,9 @@ class TestAppium(unittest.TestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Remote(appium_server_url, options=AppiumOptions().load_capabilities(capabilities))
         self.driver.implicitly_wait(50)
+        self.homepage = HomePage(self.driver)
+        self.product_page = ProductPage(self.driver)
+        self.cart_page = CartPage(self.driver)
 
         # wait - adding poll frequency and ignored exceptions
         self.wait = WebDriverWait(self.driver, 10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException])
@@ -51,83 +54,42 @@ class TestAppium(unittest.TestCase):
         time.sleep(3)
 
         # scroll to and select the country name
-        country_select = self.wait.until(
-            EC.presence_of_element_located((AppiumBy.ID, "com.androidsample.generalstore:id/spinnerCountry")))
-        country_select.click()
-        self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
-                                 f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("{locators.COUNTRY_NAME}"))')
-        country_select_entry = self.wait.until(EC.presence_of_element_located(
-            (AppiumBy.XPATH, f"//android.widget.TextView[@text='{locators.COUNTRY_NAME}']")))
-        country_select_entry.click()
+        self.homepage.select_country()
 
         # enter test user information (name and gender)
-        user_entry = self.wait.until(
-            EC.presence_of_element_located((AppiumBy.ID, "com.androidsample.generalstore:id/nameField")))
-        user_entry.send_keys(f"{locators.USER_NAME}")
-        gender_entry_f = self.wait.until(
-            EC.presence_of_element_located((AppiumBy.ID, "com.androidsample.generalstore:id/radioFemale")))
-        gender_entry_f.click()
+        self.homepage.enter_user_name()
+        self.homepage.select_gender("F")
 
         # tap "Let's Shop" button
-        lets_shop_button = self.wait.until(
-            EC.presence_of_element_located((AppiumBy.ID, "com.androidsample.generalstore:id/btnLetsShop")))
-        lets_shop_button.click()
+        self.homepage.click_shop_button()
 
         # verify that product screen is displayed
-        product_title_bar = self.wait.until(
-            EC.presence_of_element_located((AppiumBy.XPATH, "//android.widget.TextView[@text='Products']")))
-        self.assertEqual(product_title_bar.text, "Products")
+        self.assertEqual(self.product_page.get_text(*ProductPageLocators.PRODUCT_TITLE_BAR), "Products")
 
         # scroll down and add two items to cart
-        for item_name in locators.ITEMS:
-            self.driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,
-                                     f'new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text("{item_name}"))')
-            item_el = self.wait.until(
-                EC.presence_of_element_located((AppiumBy.XPATH, f"//android.widget.TextView[@text='{item_name}']")))
-            self.assertEqual(item_el.text, item_name)
-
-            # add item to cart
-            add_item_layout = self.driver.find_element(AppiumBy.XPATH,
-                                                       f"//android.widget.TextView[@text='{item_name}']/following-sibling::android.widget.LinearLayout[2]")
-            add_item_button = add_item_layout.find_element(AppiumBy.XPATH,
-                                                           "//android.widget.TextView[@text='ADD TO CART']")
-            self.assertEqual(add_item_button.text, "ADD TO CART")
-            add_item_button.click()
+        for item_name in ProductPageLocators.ITEMS:
+            added_item = self.product_page.add_item(item_name)
+            self.assertEqual(added_item.text, item_name)
             time.sleep(1)
 
         # tap the cart button
-        cart_button = self.driver.find_element(AppiumBy.ID, "com.androidsample.generalstore:id/appbar_btn_cart")
-        cart_button.click()
+        self.product_page.click_cart_button()
 
         # verify that the cart screen is displayed and that the correct items have been added
-        cart_title_bar = self.wait.until(
-            EC.presence_of_element_located((AppiumBy.ID, "com.androidsample.generalstore:id/toolbar_title")))
-        self.assertEqual(cart_title_bar.text, "Cart")
+        self.assertEqual(self.product_page.get_text(*CartPageLocators.TITLE_BAR), "Cart")
 
         # verify that the total purchase amount is displayed (should be the sum of the prices of the two items added to the cart)
         total_cost = 0
-        for item_name in locators.ITEMS:
-            # look for the price
-            price_layout = self.driver.find_element(AppiumBy.XPATH,
-                                                    f"//android.widget.TextView[@text='{item_name}']/following-sibling::android.widget.LinearLayout[2]")
-            price = price_layout.find_element(AppiumBy.XPATH, "//android.widget.TextView")
-            total_cost += float(price.text[1:])
+        for item_name in ProductPageLocators.ITEMS:
+            total_cost += self.cart_page.get_item_price(item_name)
 
-        total_purchase_amount = self.driver.find_element(AppiumBy.XPATH,
-                                                         f"//android.widget.TextView[@text='Total Purchase Amount:  ']/following-sibling::android.widget.TextView[1]")
-        total_purchase_amount = float(total_purchase_amount.text[1:])
-
-        self.assertEqual(total_purchase_amount, total_cost)
+        self.assertEqual(self.cart_page.get_purchase_price(), total_cost)
 
         # tap "Send me emails for discounts" checkbox
-        email_checkbox = self.wait.until(EC.presence_of_element_located((AppiumBy.XPATH,
-                                                                         "//android.widget.CheckBox[@text='Send me e-mails on discounts related to selected products in future']")))
-        email_checkbox.click()
+        self.cart_page.click_email_checkbox()
 
         # tap "Visit to website" button
-        website_button = self.wait.until(
-            EC.presence_of_element_located((AppiumBy.ID, "com.androidsample.generalstore:id/btnProceed")))
-        website_button.click()
+        self.cart_page.click_proceed_button()
 
         time.sleep(3)
 
